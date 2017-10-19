d7a0d09cc7dd6e9127f25567ebfd9facfa9ac114
mattkirchner <mattkirchner@10.30.20.240>
2017-10-19 15:44:19 -0500

    worked on lab 1

--- src/lab1/Challenge1.java
+++ src/lab1/Challenge1.java
@@ -23,19 +23,29 @@ public class Challenge1 {
         Challenge1 app = new Challenge1();
         
         String fullName = JOptionPane.showInputDialog("Enter full name (use Format: first last):");
-        String lastName = app.extractLastName(fullName);
-        String msg = "Your last name is: " + lastName;
-        JOptionPane.showMessageDialog(null, msg);
+        
+        try {
+            String lastName = app.extractLastName(fullName);
+            String msg = "Your last name is: " + lastName;
+            JOptionPane.showMessageDialog(null, msg);
+        } catch(NullPointerException ne){
+            System.out.println(ne.getMessage());
+        } catch(ArrayIndexOutOfBoundsException aioe){
+            System.out.println("You didn't use a space to seperate the words, try again.");
+        }
     }
     
     // write the code to extract the lastName from the fullName
     // Use exception handling to prevent a crash in the event that fullName
     // is null or empty. Throw the exception to the calling method. and handle
     // it there.
-    public String extractLastName(String fullName) {
+    public String extractLastName(String fullName) throws NullPointerException, ArrayIndexOutOfBoundsException{
         String lastName = null;
-        
-        // Your code goes here.
+        if(fullName == null || fullName.isEmpty()){
+            throw new NullPointerException("Must enter your name properly, please try again.");
+        }
+        String[] nameParts = fullName.split(" ");
+        lastName = nameParts[LAST_NAME_IDX];
         
         return lastName;
     }
